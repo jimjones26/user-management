1481a5a87fdad0c192413f1f8a49e320bbbe96c0
jjones <jimmy.jones@tutanota.com>
2025-03-16 18:58:29 -0600

    Add JwtTokenFilter import to SecurityConfig class

--- backend/src/main/java/com/uxfx/usermanagement/config/SecurityConfig.java
+++ backend/src/main/java/com/uxfx/usermanagement/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.uxfx.usermanagement.security.JwtTokenFilter;
 
 @Configuration
 @EnableWebSecurity
