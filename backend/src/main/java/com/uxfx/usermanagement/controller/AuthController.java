51f0d3d0bf8d42ffbfddf367228c524699c4367b
jjones <jimmy.jones@tutanota.com>
2025-03-17 17:03:25 -0600

    Remove autowired annotation from AuthController

--- backend/src/main/java/com/uxfx/usermanagement/controller/AuthController.java
+++ backend/src/main/java/com/uxfx/usermanagement/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.uxfx.usermanagement.controller;
 import com.uxfx.usermanagement.dto.*;
 import com.uxfx.usermanagement.service.AuthService;
 import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
