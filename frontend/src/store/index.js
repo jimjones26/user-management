b6c4332f6108ed26f8d36ceda7d1a66849961ff7
jjones <jimmy.jones@tutanota.com>
2025-03-18 15:07:15 -0600

    Fix redux-thunk import to use named import

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import authReducer from './reducers/authReducer';
 import userReducer from './reducers/userReducer';
 
