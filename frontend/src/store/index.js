0741e854fba79660e16a09c8e17baa24697ee18f
jjones <jimmy.jones@tutanota.com>
2025-03-18 14:54:06 -0600

    Simplify Redux store configuration by removing configureStore function

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,25 +4,17 @@ import thunk from 'redux-thunk';
 import authReducer from './reducers/authReducer';
 import userReducer from './reducers/userReducer';
 
-// Create and export the store setup function
-const configureStore = () => {
-  const rootReducer = combineReducers({
-    auth: authReducer,
-    user: userReducer
-  });
+const rootReducer = combineReducers({
+  auth: authReducer,
+  user: userReducer
+});
 
-  // Explicitly apply thunk middleware first
-  const middlewares = [thunk];
-  
-  const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(...middlewares))
-  );
+// Explicitly apply thunk middleware first
+const middlewares = [thunk];
 
-  return store;
-};
-
-// Create and export the store instance
-const store = configureStore();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 export default store;
