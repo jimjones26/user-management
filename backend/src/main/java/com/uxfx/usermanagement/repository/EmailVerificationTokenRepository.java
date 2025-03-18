8c1fcd94e0a467c3ac44e0985266bf146d620299
jjones <jimmy.jones@tutanota.com>
2025-03-18 11:01:05 -0600

    Add findByToken method to EmailVerificationTokenRepository

--- backend/src/main/java/com/uxfx/usermanagement/repository/EmailVerificationTokenRepository.java
+++ backend/src/main/java/com/uxfx/usermanagement/repository/EmailVerificationTokenRepository.java
@@ -3,5 +3,8 @@ package com.uxfx.usermanagement.repository;
 import com.uxfx.usermanagement.model.EmailVerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
+    Optional<EmailVerificationToken> findByToken(String token);
 }
