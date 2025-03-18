c6e6a87f96f6adb14a42430869da39a95dbb67df
jjones <jimmy.jones@tutanota.com>
2025-03-18 12:20:08 -0600

    Refactor database configuration to use H2 for tests

--- backend/src/test/java/com/uxfx/usermanagement/config/TestConfig.java
+++ backend/src/test/java/com/uxfx/usermanagement/config/TestConfig.java
@@ -3,7 +3,6 @@ package com.uxfx.usermanagement.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -16,25 +15,12 @@ import javax.sql.DataSource;
 public class TestConfig {
     
     /**
-     * Primary test database configuration
-     * This will be used instead of the application's main datasource during tests
+     * Primary test database configuration using H2 in-memory database
+     * This provides faster test execution without requiring external database setup
      */
     @Bean
     @Primary
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5433/user_management_test");
-        dataSource.setUsername("jjones");
-        dataSource.setPassword("password");
-        return dataSource;
-    }
-    
-    /**
-     * In-memory database for faster tests that don't require PostgreSQL
-     */
-    @Bean(name = "inMemoryDataSource")
-    public DataSource inMemoryDataSource() {
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("classpath:schema.sql")
